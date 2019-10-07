602441702bebbfa3cce7d59c61a48c051f5423d2
Easton-CYBER <55762101+Easton-CYBER@users.noreply.github.com>
2019-10-07 16:42:41 -0400

    Update AutoBase.java

--- src/AutoBase.java
+++ src/AutoBase.java
@@ -1,12 +1,6 @@
-import javax.swing.*;
-
 public class AutoBase{
     public static void main(String[] args){
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new TableSQL();
-            }
-        });
-
+        Login login = new Login();
+        login.Log();
     }
 }
