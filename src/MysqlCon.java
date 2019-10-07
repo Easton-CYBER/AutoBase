61500ff700a3608b025e0cd0e04988c028f717c5
Easton-CYBER <55762101+Easton-CYBER@users.noreply.github.com>
2019-10-07 16:42:10 -0400

    Update MysqlCon.java

--- src/MysqlCon.java
+++ src/MysqlCon.java
@@ -1,12 +1,5 @@
-import java.sql.*;
-
 public class MysqlCon {
-    
-    public Boolean resultset;
-    public ResultSet rs;
-    public Statement stmnt;
-    public Connection con;
-    public String url1 = "jdbc:mysql://localhost:3306/customer_Info";
+    public String url = "jdbc:mysql://localhost:3306/customer_Info";
     public String user = "root";
     public String password = "!1Swamigee";
     
